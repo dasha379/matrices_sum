e4a130ae56766a44a2cd3c7b32229c4c34a1f361
dasha379 <hjfktd@yandex.ru>
2025-03-03 19:51:59 +0300

    Delete openCL_pr/openCL_pr directory

--- openCL_pr/openCL_pr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dasha/openCL_pr
-# Build directory: /home/dasha/openCL_pr/openCL_pr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
